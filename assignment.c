b48bc9062353ededdad73811de75b9aed9b20f4f
Florian Mataigne <f.mataigne@student.uliege.be>
2021-02-11 16:30:41 +0100

    assignment file prints hello

---
+++ assignment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[])
+{
+    printf("hello\n");
+    return 0;
+}#include <stdio.h>
+
+int main(int argc, char const* argv[])
+{
+    printf("hello\n");
+    return 0;
+}
